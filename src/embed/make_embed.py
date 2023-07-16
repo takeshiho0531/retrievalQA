5591877ea94195b9927574654d83bd803bd20faa
takeshiho0531 <kawadatakeshiho@gmail.com>
2023-07-17 02:36:57 +0900

    feat: embedding作るscriptできた

--- src/embed/make_embed.py
+++ src/embed/make_embed.py
@@ -1,19 +1,15 @@
-import sys
+import os
 import re
+import sys
 from glob import glob
 from typing import Optional
 
-from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.document_loaders import PDFMinerLoader
-from langchain.text_splitter import (  RecursiveCharacterTextSplitter,
-    CharacterTextSplitter,
-)
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
-import tiktoken
 
-import os
-os.environ["OPENAI_API_KEY"]="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"
-
+os.environ["OPENAI_API_KEY"] = "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"
 
 def create_embd_db(parent_regex: str):
     documents_pdf = []
@@ -22,50 +18,29 @@ def create_embd_db(parent_regex: str):
         print(path)
         loader: Optional[PDFMinerLoader] = None
 
-        token_num_list=[]
-
         if re.match(".+\.(pdf)$", path):
             loader = PDFMinerLoader(file_path=path)
             documents_pdf += loader.load()
 
-            data=loader.load()
-            print(data)
-            text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10, separator="\n\n")
-            text=text_splitter.split_documents(data)
-            enc = tiktoken.get_encoding("cl100k_base")
-            tokens = enc.encode(str(text))
+    texts = []
+    text_splitter = CharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=10, separator="\n\n"
+    )
+    texts += text_splitter.split_documents(documents_pdf)
 
-            if len(tokens)>500:
-                token_num_list.append(path)
-        print(token_num_list)
-
-
-    #texts = []
-    #text_splitter = RecursiveCharacterTextSplitter(chunk_size=50, chunk_overlap=0)
-    #texts += text_splitter.split_documents(documents_pdf)
-
-    #embeddings = OpenAIEmbeddings(
-        #model="text-embedding-ada-002",
-        #chunk_size=1,
-    #)
-    #db = FAISS.from_documents(texts, embeddings)
-    #return db
-
+    embeddings = OpenAIEmbeddings()  # type: ignore
+    db = FAISS.from_documents(texts, embeddings)
+    return db
 
 
 def main(db_path: str) -> None:
-    embeddings = OpenAIEmbeddings(
-        model="text-embedding-ada-002",
-        chunk_size=1,
-    )
     db = create_embd_db(
-        parent_regex="./data/msm_data/spec_json/split/統合/*.json",
+        parent_regex="./data/*.pdf",
     )
     db.save_local(db_path)
 
 
 if __name__ == "__main__":
-    #main(
-        #db_path="./data/msm_data/spec_json/faiss_index/merged",
-    #)
-    create_embd_db("./data/*.pdf")
+    main(
+        db_path="./data_faiss",
+    )
